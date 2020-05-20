0786d630d1a2fe51183659dd50fc8ebf0ca95f80
stevenmcdonald <engineers2526@gmail.com>
2020-05-19 19:40:13 -0500

    Add developer.js model class

---
+++ src/models/developer.js
@@ -0,0 +1,10 @@
+export class Developer {
+    constructor(firstName, lastName, typeOfDeveloer, username, password, id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.typeOfDeveloper = typeOfDeveloer; 
+        this.id = id;
+    }
+}

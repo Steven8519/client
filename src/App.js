98444d0e4614c2156a5b813d177f71b5b42e44d1
stevenmcdonald <engineers2526@gmail.com>
2020-06-04 14:19:30 -0500

    added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import './App.css';
+import {Router, Link, Switch, Redirect, Route} from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import HomePage from './pages/home/home.page';
+import LoginPage from './pages/login/login.page';
+import RegisterPage from './pages/register/register.page';
 
-function App() {
-  return (
-    <h1>React App</h1>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      history: createBrowserHistory(),
+    };
+  }
+
+  render() {
+    const {history} = this.state;
+    return(
+      <Router history={history}>
+        <div>
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route exact path="/home" component={HomePage} />
+              <Route exact path="/login" component={LoginPage} />
+              <Route exact path="/home" component={RegisterPage} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
-
-export default App;
